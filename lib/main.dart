88a18b372661ac3c7ca8055c75a968d5ff8bdb80
Maxx <max13.bl@gmail.com>
2025-05-24 12:59:42 +0200

    ctn

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:asiimov/auth/login_or_register.dart';
+import 'package:asiimov/firebase_options.dart';
 import 'package:asiimov/themes/light_mode.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
