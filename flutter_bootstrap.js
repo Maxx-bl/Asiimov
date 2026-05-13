e0eb755998b2a56c2398b5488192b75a59eb2151
Maxx-bl <84576379+Maxx-bl@users.noreply.github.com>
2026-05-13 22:41:03 +0000

    Deploying to gh-pages from @ Maxx-bl/Asiimov@093658ce01adcaf1a3e43d6e5e43f48ff3a52920 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1357337239" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "958378479" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
